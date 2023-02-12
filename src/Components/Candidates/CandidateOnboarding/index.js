0fb094460a8a55b57358dbdd0cc694589aff9ea8
Kunal Charde <kunalcharde1@gmail.com>
2023-02-12 12:22:17 +0530

    candidate onboarding component started

--- src/Components/Candidates/CandidateOnboarding/index.js
+++ src/Components/Candidates/CandidateOnboarding/index.js
@@ -1,11 +1,82 @@
-import React from 'react'
-
+import React from "react";
+import { Label } from "@mui/icons-material";
+import { Button, Grid, TextField } from "@mui/material";
+import "./style.css";
+import Autocomplete from '@mui/material/Autocomplete';
+import CountrySelect from "../../../Constants";
 const CandidateOnboarding = () => {
   return (
-    <div>
-      
+    <div className="candidate-onboarding-container">
+      onv
+      <div className="candidate-onboarding-form">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className="flex">
+            <CountrySelect/>
+            <TextField
+              id="outlined-basic"
+              label="Phone"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              required
+              size="Normal"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CandidateOnboarding
+export default CandidateOnboarding;
